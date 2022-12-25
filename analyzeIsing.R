da0050610b217afb334c9b6e7a730a56e26dac4f
andrewjholbrook <aholbroo@g.ucla.edu>
2022-12-24 16:53:04 -0800

    convergence fig

--- analyzeIsing.R
+++ analyzeIsing.R
@@ -5,20 +5,33 @@ df <- readr::read_table2("~/qpMCMC/Ising2dLogProbs.txt", col_names = FALSE)
 
 df <- df[,1:2]
 
-df$Proposals <- factor(df$X1)
-df$LogProbs  <- df$X2
-df$Iterations <- rep(seq.int(from=1,to=1000000),10)
-df <- df[,3:5]
-df <- df[df$Iterations %in% seq.int(from=1,to=1000000,by=1000),]
+# temporary
+df <- df[df$X1 %in% c(4,8,16,32,64,128,256,512,1024),]
 
-gg <- ggplot(df,aes(x=Iterations,y=LogProbs,color=Proposals)) +
-  geom_line()
+
+df$Proposals <- factor(df$X1)
+df$`Parallel\nMCMC\nproposals` <- forcats::fct_rev(df$Proposals)
+df$LogProbs  <- df$X2
+df$Iterations <- rep(seq.int(from=1,to=10000000,by=1000),9)
+df <- df[,3:6]
+
+pal <- wesanderson::wes_palette("Zissou1",9,type="continuous")
+
+gg <- ggplot(df,aes(x=Iterations,y=LogProbs,color=`Parallel\nMCMC\nproposals`)) +
+  geom_line() +
+  scale_color_manual(values = pal) +
+  ylab("Unnormalized log-probabilities") + xlab("MCMC iterations") +
+  ggtitle("Convergence for a 2D Ising model on 500-by-500 lattice") +
+  theme_bw()
 gg
 
-gg2 <- ggplot(df,aes(x=Iterations,y=Distance,color=Proposals)) +
-  geom_line() +
-  ylim(c(124000,126000)) + xlim(c(200000,1000000))
-gg2
+ggsave("~/qpMCMC/figures/Ising2dFig.pdf",device = "pdf",width = 7,height=4)
+system2(command = "pdfcrop",
+        args    = c("~/qpMCMC/figures/Ising2dFig.pdf",
+                    "~/qpMCMC/figures/Ising2dFig")
+)
+
+
 
 coda::effectiveSize(df$Distance[df$Proposals==4 & df$Iterations>=200000])
 coda::effectiveSize(df$Distance[df$Proposals==8 & df$Iterations>=200000])
