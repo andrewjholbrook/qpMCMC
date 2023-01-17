c4b93409f70a6b23b8271998b80c514bf916d6bb
andrewjholbrook <aholbroo@g.ucla.edu>
2023-01-17 10:03:01 -0800

    blackhole figure

--- analyzeBlackHole.R
+++ analyzeBlackHole.R
@@ -2,35 +2,66 @@ setwd("~/qpMCMC")
 
 library(readr)
 library(coda)
+library(ggplot2)
+library(reshape2)
 # 
-df <- read_table2("blackHoleResultsCenteredPrior.txt",col_names = FALSE)
+df <- read_table2("blackHoleResults.txt",col_names = FALSE)
 df <- as.matrix(df[,1:6])
-# for(i in 1:7){
-#   df[,i] <- unlist(df[,i])
-# }
-#effectiveSize(df[4000:8000,4])
-plot(df[500:856,5],type="l")
 
-effectiveSize(df[500:856,])
+effectiveSize(df[2501:5000,]) # remove first half as burning
+# X1          X2          X3          X4 
+# 0.000000  257.077281 1578.662332  257.608159 
+# X5          X6 
+# 2500.000000    1.500075 
 
-df <- read_table2("blackHoleResultsSinglePrec.txt",col_names = FALSE)
-df <- as.matrix(df[,1:5])
-# for(i in 1:7){
-#   df[,i] <- unlist(df[,i])
-# }
-#effectiveSize(df[4000:8000,4])
-plot(df[,4],type="l")
+
+#plot(df[2501:5000,5],type="l")
+
+
+1024*20000000/df[5000,6] # 10.36 speedup
 
 
 
 
 
-# results <- readRDS("~/qpMCMC/blackHole.rds")
-# 
-# 1024*20000000/results[[4]]
-# plot(results[[8]],type="l")
-# 
-# imgLong <- melt(results[[1]][[1000]])
-# remove(results)
-# ggplot(imgLong, aes(x = Var2, y = Var1)) +
-#   geom_raster(aes(fill=value))
+
+results <- readRDS("~/qpMCMC/blackHoleResults2.rds")
+
+# get mode
+imgLong <- melt(results[[1]])
+for(i in 2:26) {
+  imgLong <- imgLong + melt(results[[i]])
+}
+imgLong <- round(imgLong / 26)
+remove(results)
+gg2 <- ggplot(imgLong, aes(x = Var2, y = Var1)) +
+  geom_raster(aes(fill=value),show.legend = FALSE) +
+  scale_fill_gradient(low = "black",high="white") +
+  ggtitle("Pixelwise posterior mode") +
+  scale_x_continuous(expand = c(0,0) ) +
+  scale_y_continuous(expand = c(0,0) ) +
+  theme_void()
+gg2
+remove(imgLong)
+
+img <- readRDS("~/qpMCMC/figures/blackHoleIntensity.rds")
+imgLong <- melt(img)
+remove(img)
+gg1 <- ggplot(imgLong, aes(x = Var2, y = Var1)) +
+  geom_raster(aes(fill=value),show.legend = FALSE) +
+  scale_fill_gradient(low = "black",high="white") +
+  ggtitle("Intensities from Sagittarius A* image") +
+  scale_x_continuous(expand = c(0,0) ) +
+  scale_y_continuous(expand = c(0,0) ) +
+  theme_void()
+gg1
+
+library(ggpubr)
+
+ggsave(ggarrange(gg1, gg2, ncol = 2, nrow = 1),filename = "blackHole.pdf",device = "pdf",path = "~/qpMCMC/figures/",dpi = "retina",
+       width=9,height=3)
+
+system2(command = "pdfcrop",
+        args    = c("~/qpMCMC/figures/blackHole.pdf",
+                    "~/qpMCMC/figures/blackHole.pdf")
+)
